6087d4bad0412f64281295a8257de52b5c8ee0bd
Teodor Madzovski <ted.madzovski@gmail.com>
2023-03-09 18:12:05 +0100

    3 commit
    adding images

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,19 @@
 module.exports = {
   content: ["./dist/*.{html,js}"],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily:{
+        'title':['Poppins', 'sans-serif'],
+        'body':['Poppins', 'sans-serif']
+      },
+      colors:{
+        'pri': '##F2F2F2',
+        'sec': '##22789D',
+        'gen': '#000000',
+        'our-bg': '#FFFFFF'
+
+      }
+    },
   },
   plugins: [],
 }
